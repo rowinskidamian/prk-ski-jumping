673d87a4105377aa85def0e9b74474049c5920b0
Damian Rowiński <damian.rowinski@gmail.com>
2021-03-09 19:38:26 +0100

    added tournament database

--- src/main/java/prk/ski/jumping/model/domain/TournamentJumperResultDB.java
+++ src/main/java/prk/ski/jumping/model/domain/TournamentJumperResultDB.java
@@ -1,5 +1,47 @@
 package main.java.prk.ski.jumping.model.domain;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class TournamentJumperResultDB {
+
+    private static long currentDbIndex = 0;
+    private static HashMap<Long, TournamentJumperResult> database;
+
+    public TournamentJumperResultDB() {
+        if(database != null) database = new HashMap<>();
+    }
+
+    public void create(TournamentJumperResult tjr) {
+        tjr.setDatabaseId(currentDbIndex);
+        database.put(currentDbIndex, tjr);
+        currentDbIndex++;
+    }
+
+    public Optional<TournamentJumperResult> getById(long id) {
+        boolean isPresent = database.containsKey(id);
+        return isPresent ? Optional.of(database.get(id)) : Optional.empty();
+    }
+
+    public List<TournamentJumperResult> getAll() {
+        return database.keySet().stream()
+                .map(database::get)
+                .collect(Collectors.toList());
+    }
+
+    public void update(TournamentJumperResult tjr, long id) {
+        Optional<TournamentJumperResult> optionalResult = getById(id);
+        optionalResult.orElseThrow(() -> new NoSuchElementException("There is no record for given id."));
+        database.put(id, tjr);
+    }
+
+    public void delete(TournamentJumperResult tjr, long id) {
+        Optional<TournamentJumperResult> optionalResult = getById(id);
+        optionalResult.orElseThrow(() -> new NoSuchElementException("There is no record for given id."));
+        database.remove(id, tjr);
+    }
 
 }
