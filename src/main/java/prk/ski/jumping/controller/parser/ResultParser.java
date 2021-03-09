8628e61083e587e9ba5dbf5e9cf7eaba63596bd2
Damian Rowiński <damian.rowinski@gmail.com>
2021-03-09 20:17:29 +0100

    added parser service

--- src/main/java/prk/ski/jumping/controller/parser/ResultParser.java
+++ src/main/java/prk/ski/jumping/controller/parser/ResultParser.java
@@ -1,5 +1,14 @@
 package main.java.prk.ski.jumping.controller.parser;
 
+import main.java.prk.ski.jumping.model.domain.TournamentJumperResult;
+import main.java.prk.ski.jumping.model.domain.TournamentWorldCup;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ResultParser {
 
+    public List<TournamentJumperResult> getResultListFor(TournamentWorldCup tournamentWorldCup) {
+        return new ArrayList<>();
+    }
 }
--- src/main/java/prk/ski/jumping/model/dao/TournamentWorldCupDao.java
+++ src/main/java/prk/ski/jumping/model/dao/TournamentWorldCupDao.java
@@ -1,5 +1,8 @@
 package main.java.prk.ski.jumping.model.dao;
 
+import main.java.prk.ski.jumping.model.domain.TournamentWorldCup;
+
 public interface TournamentWorldCupDao {
 
+    public void create(TournamentWorldCup twc);
 }
