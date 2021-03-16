00f8ba079692c4f009123ef29acb015cbcbb53e9
Radosław Parol <radoslaw.parol95@gmail.com>
2021-03-16 18:31:32 +0100

    add jumper/country/tournamentWorldCup model

--- src/main/java/prk/ski/jumping/model/dao/TournamentWorldCupDao.java
+++ src/main/java/prk/ski/jumping/model/dao/TournamentWorldCupDao.java
@@ -5,7 +5,14 @@ import prk.ski.jumping.model.domain.TournamentWorldCup;
 import java.util.List;
 
 public interface TournamentWorldCupDao {
+    public void create(TournamentWorldCup cup);
 
-    public void create(TournamentWorldCup twc);
+    public TournamentWorldCup getById(long id);
+
     public List<TournamentWorldCup> getAll();
+
+    public void update(TournamentWorldCup cup, long id);
+
+    public void delete(TournamentWorldCup cup, long id);
+
 }
--- src/main/java/prk/ski/jumping/model/domain/Country.java
+++ src/main/java/prk/ski/jumping/model/domain/Country.java
@@ -1,5 +1,74 @@
 package prk.ski.jumping.model.domain;
 
-public class Country {
+import java.util.Objects;
 
+public class Country {
+    private long id;
+    private int goldMedals;
+    private int silverMedals;
+    private int bronzeMedals;
+
+    public Country(long id, int goldMedals, int silverMedals, int bronzeMedals) {
+        this.id = id;
+        this.goldMedals = goldMedals;
+        this.silverMedals = silverMedals;
+        this.bronzeMedals = bronzeMedals;
+    }
+
+    // getters
+    public long getId() {
+        return id;
+    }
+
+    public int getGoldMedals() {
+        return goldMedals;
+    }
+
+    public int getSilverMedals() {
+        return silverMedals;
+    }
+
+    public int getBronzeMedals() {
+        return bronzeMedals;
+    }
+
+    // setters
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setGoldMedals(int medals) {
+        this.goldMedals = medals;
+    }
+
+    public void setSilverMedals(int medals) {
+        this.silverMedals = medals;
+    }
+
+    public void setBronzeMedals(int medals) {
+        this.bronzeMedals = medals;
+    }
+
+    public boolean equals(Object other) {
+        if (this == other)
+            return true;
+        if (other == null)
+            return false;
+        if (getClass() != other.getClass())
+            return false;
+        Country country = (Country) other;
+
+        return id == country.id && goldMedals == country.goldMedals && silverMedals == country.silverMedals
+                && bronzeMedals == country.bronzeMedals;
+    }
+
+    public int hashCode() {
+        return (int) Objects.hash(id, goldMedals, silverMedals, bronzeMedals);
+    }
+
+    public String toString() {
+        String country = "Country {" + "id = " + id + ", goldMedals = " + goldMedals + ", silverMedals = "
+                + silverMedals + ", bronzeMedals = " + bronzeMedals + "}";
+        return country;
+    }
 }
