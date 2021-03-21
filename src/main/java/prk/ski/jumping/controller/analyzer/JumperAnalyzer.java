293a75da939f2d5fd71280961e1d178bff67f9be
Damian Rowiński <damian.rowinski@gmail.com>
2021-03-21 18:11:39 +0100

    method for jumperAnalyzer

--- src/main/java/prk/ski/jumping/controller/analyzer/JumperAnalyzer.java
+++ src/main/java/prk/ski/jumping/controller/analyzer/JumperAnalyzer.java
@@ -1,5 +1,22 @@
 package prk.ski.jumping.controller.analyzer;
 
+import prk.ski.jumping.model.domain.Jumper;
+import prk.ski.jumping.model.domain.TournamentJumperResult;
+import prk.ski.jumping.model.domain.TournamentWorldCup;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JumperAnalyzer {
+
+    public List<Jumper> getJumperAnalysis(List<TournamentJumperResult> resultList, List<TournamentWorldCup> tournamentList) {
+        List<Jumper> jumperList = new ArrayList<>();
+        int goldMedals = 0;
+        int silverMedals = 0;
+        int bronzeMedals = 0;
+
+
+        return jumperList;
+    }
 
 }
