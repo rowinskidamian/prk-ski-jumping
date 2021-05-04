91388b6699f076bca558f7b0dc298567ae91b1f6
Radosław Parol <radoslaw.parol95@gmail.com>
2021-05-04 18:10:42 +0200

    create web.xml

--- src/main/java/prk/ski/jumping/controller/utils/DbUtilities.java
+++ src/main/java/prk/ski/jumping/controller/utils/DbUtilities.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtilities {
-    static final String DATABASE_URL = "jdbc:mysql://localhost:3306";
-    static final String DATABASE_USERNAME = "admin";
-    static final String DATABASE_PASSWORD = "password";
+    static final String DATABASE_URL = "jdbc:mysql://localhost:3306/prk_ski_jumping?serverTimezone=UTC";
+    static final String DATABASE_USERNAME = "root";
+    static final String DATABASE_PASSWORD = "root";
 
     public static Connection connectToDatabase() throws SQLException {
         return DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
