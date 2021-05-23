94e81930819958aae199ef788ca4144e9eb33ef7
rowinskidamian <damian.rowinski@gmail.com>
2021-05-23 19:47:57 +0200

    added json creation for chart and test in putting in JS script

--- src/main/java/prk/ski/jumping/controller/utils/DbUtilities.java
+++ src/main/java/prk/ski/jumping/controller/utils/DbUtilities.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DbUtilities {
     static final String DATABASE_URL = "jdbc:mysql://localhost:3306/prk_ski_jumping?serverTimezone=UTC";
     static final String DATABASE_USERNAME = "root";
-    static final String DATABASE_PASSWORD = "root";
+    static final String DATABASE_PASSWORD = "coderowo";
 
     public static Connection connectToDatabase() throws SQLException {
         DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
